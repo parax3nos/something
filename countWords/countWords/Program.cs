5641ae460457d69f6fe62f1f9cc2105e5151da8a
parax3nos <125305797+parax3nos@users.noreply.github.com>
2024-03-23 16:33:17 +0700

    Add files via upload

---
+++ countWords/countWords/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace countWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int summ = 0;
+            string path = @"C:\Users\Evgeny\Desktop\ТИ\ST.txt";
+            string text = File.ReadAllText(path);
+
+            char[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            int[] counts = new int[alphabet.Length];
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (alphabet[i] == text[j])
+                        counts[i]++;
+                }
+            }
+
+            for (int i = 0; i < alphabet.Length - 33; i++)
+            {
+                Console.WriteLine($"{alphabet[i]} {counts[i] + counts[i + 33]}");
+            }
+
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                summ += counts[i];
+            }
+            Console.WriteLine($"summ = {summ}");
+            Console.ReadLine();
+        }
+    }
+}
